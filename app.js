e6e5847a44f14c6995e4fbfdd3c955a8aaf8e9c2
Paul John Chamberlain <114663999+pauljohnchamberlain@users.noreply.github.com>
2023-05-11 11:34:22 +0930

    Implemented CORS

--- app.js
+++ app.js
@@ -28,10 +28,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
 
-// Implement CORS
+// Implement CORS - Cross Origin Resource Sharing
 app.use(cors());
+// Access-Control-Allow-Origin *
+// app.use(cors({ origin: 'https://www.natours.com' }));
 
+// Preflight phase
 app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
