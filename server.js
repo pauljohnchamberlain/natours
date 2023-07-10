c8354a0ade9cb038361c1021c3efffcae03b05fd
Paul John Chamberlain <114663999+pauljohnchamberlain@users.noreply.github.com>
2023-07-10 11:03:15 +0930

    async/await moongoose.connect

--- server.js
+++ server.js
@@ -17,22 +17,27 @@ const DB = process.env.DATABASE.replace(
 );
 
 // mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-mongoose.connect(DB).then(() => {
-  console.log('DB connection successful!');
-  const port = process.env.PORT || 3000;
+(async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log('DB connection successful!');
+    const port = process.env.PORT || 3000;
 
-  const server = app.listen(port, () =>
-    console.log(`App running on port ${port}...`)
-  );
+    const server = app.listen(port, () =>
+      console.log(`App running on port ${port}...`)
+    );
 
-  process.on('unhandledRejection', (err) => {
-    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-    console.error(err.name, err.message);
-    server.close(() => {
-      process.exit(1);
+    process.on('unhandledRejection', (err) => {
+      console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+      console.error(err.name, err.message);
+      server.close(() => {
+        process.exit(1);
+      });
     });
-  });
-});
+  } catch (error) {
+    console.error('DB connection failed:', error);
+  }
+})();
 
 // heroku logs --tail
 process.on('SIGTERM', () => {
